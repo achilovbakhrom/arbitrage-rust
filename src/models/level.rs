15a65719dfcba931b75a3fc7b704ffbad543a9fd
mailbak36 <mailbak36@gmail.com>
2025-05-29 04:25:03 +0500

    a bit cleaning

--- src/models/level.rs
+++ src/models/level.rs
@@ -1,14 +1,3 @@
-use crate::enums::side::Side;
-
-/// Single level update coming from the SBE socket
-#[derive(Debug, Clone)]
-pub struct LevelUpdate {
-    pub price: f64,
-    pub quantity: f64,
-    pub side: Side,
-    pub update_id: u64,
-}
-
 /// A price level in the orderbook
 #[derive(Debug, Clone)]
 pub struct Level {
